49a6a76352b6de549f5ba29dfa56708194f72486
Lorien Smyer <lorien@Loriens-MacBook-Air.local>
2015-05-14 02:45:26 -0700

    add gradient js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-var size = 81;
-
-for (var i = 0; i < size; i++) {
+for (var i = 0; i < 81; i++) {
 	var square = document.createElement("div");
 	square.style.width = "11.1%";
     square.style.paddingBottom = "11.1%";
@@ -14,7 +12,7 @@ for (var i = 0; i < size; i++) {
     }
 } 
 
-for (var i = 0; i < size; i++) {
+for (var i = 0; i < 81; i++) {
 	randNbr1 = Math.floor(Math.random() * 255);
 	randNbr2 = Math.floor(Math.random() * 255);
 	randNbr3 = Math.floor(Math.random() * 255);
@@ -27,4 +25,27 @@ for (var i = 0; i < size; i++) {
     document.body.appendChild(square);
 }
 
-
+var color1 = 0;
+var color2 = 255;
+var color3 = 255;
+for (var i = 0; i < 81; i++) {
+    var square = document.createElement("div");
+    square.style.width = "11.1%";
+    square.style.paddingBottom = "11.1%";
+    square.style.float = "left";       
+    if (i % 2 === 0) {
+        for(var j = 0; j < 41; j++) {
+            var grad1 = "rgb(" + color1 + "," + color2 + "," + color3 + ")";
+            square.style.backgroundColor = grad1;              
+            document.body.appendChild(square);           
+        } 
+    } else {
+        for(var k = 0; k < 40; k++) {
+            var grad2 = "rgb(" + color2 + "," + color1 + "," + color3 + ")";
+            square.style.backgroundColor = grad2;    
+            document.body.appendChild(square);           
+        }
+    }
+    color1+=3;
+    color3-=8;
+}
