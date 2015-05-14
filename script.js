0621e28e24805cc49aaa15eda155008edc87d983
Lorien Smyer <lorien@Loriens-MacBook-Air.local>
2015-05-14 01:25:46 -0700

    add checkerboard js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Your JS goes here
+var size = 81;
+
+for (var i = 0; i < size; i++) {
+	var square = document.createElement("div");
+	square.style.width = "11.1%";
+    square.style.paddingBottom = "11.1%";
+    square.style.float = "left";		
+    if (i % 2 === 0) {
+ 		square.style.backgroundColor = "#000";				
+        document.body.appendChild(square);
+    } else {
+        square.style.backgroundColor="#F00";
+        document.body.appendChild(square);
+    }
+} 
+
+
