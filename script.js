f6caa6b69c5bb884f09df8038afb02fc2696067e
Lorien Smyer <lorien@Loriens-MacBook-Air.local>
2015-05-14 01:29:15 -0700

    add randomcolor js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Your JS goes here
+var size = 81;
+
+for (var i = 0; i < size; i++) {
+	randNbr1 = Math.floor(Math.random() * 255);
+	randNbr2 = Math.floor(Math.random() * 255);
+	randNbr3 = Math.floor(Math.random() * 255);
+	randColor = "rgb(" + randNbr1 + "," + randNbr2 + "," + randNbr3 + ")";
+    var square = document.createElement("div");
+    square.style.width = "11.1%";
+    square.style.paddingBottom = "11.1%";
+    square.style.float = "left";        
+    square.style.backgroundColor = randColor;              
+    document.body.appendChild(square);
+}
+
